4a283cc392187e0821480175fb8966f6520b9ffb
shulepa ivan <154735543+sunn3t@users.noreply.github.com>
2026-03-26 01:11:38 +0200

    Update 4_4.py

--- A27_2/lab 3&4/4.4/4_4.py
+++ A27_2/lab 3&4/4.4/4_4.py
@@ -1,1 +1,19 @@
+import math
 
+def solve_task_4_4():
+    print("--- Задача 4.4 ---")
+    print("Рівняння: sin(x) = x / 3 на відрізку [1.6, 3]")
+    left = 1.6
+    right = 3.0
+    for _ in range(100):
+        mid = (left + right) / 2.0
+        val = math.sin(mid) - mid / 3.0
+        if val > 0:
+            left = mid
+        else:
+            right = mid
+            
+    print(f"Корінь рівняння x ≈ {left:.6f}\n")
+
+if __name__ == '__main__':
+    solve_task_4_4()
